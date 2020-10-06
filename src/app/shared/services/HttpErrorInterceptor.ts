46d881e2117f271e0a314256c931af656f8d7037
Amir Esmaeili <amir-esmaeili@outlook.com>
2020-10-06 19:34:49 +0330

    unauthorized redirection

--- src/app/shared/services/HttpErrorInterceptor.ts
+++ src/app/shared/services/HttpErrorInterceptor.ts
@@ -1,25 +1,35 @@
-import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpErrorResponse } from '@angular/common/http';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpErrorResponse, HttpResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { catchError, tap } from 'rxjs/operators';
 import {Router} from '@angular/router';
 import {Injectable} from '@angular/core';
 import {MatSnackBar} from '@angular/material/snack-bar';
+import { UserService } from './user.service';
 
 @Injectable()
 export class HttpErrorInterceptor implements HttpInterceptor {
     constructor(private router: Router,
-                private matSnackBar: MatSnackBar) {
+                private matSnackBar: MatSnackBar,
+                private userService: UserService) {
     }
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(request)
-            .pipe(
-                catchError((error: HttpErrorResponse) => {
-                    const errorMsg = `Error Code: ${error.status},  Message: ${error.message}`;
-                    if (error.status === 401) {
-                      this.router.navigateByUrl('/login').then();
+            .pipe(tap(
+                (event: HttpEvent<any>) => {
+                    if (event instanceof HttpResponse) {
+                        // do stuff with response
                     }
-                    return throwError(errorMsg);
-                })
-            );
+                },
+                (err: any) => {
+
+                    if (err instanceof HttpErrorResponse) {
+                        if (err.status === 401) {
+
+                            this.userService.logOut();
+                            this.router.navigate(['/login']);
+                        }
+                    }
+                }
+            ));
     }
 }
